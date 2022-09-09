6f715914cc79ef85023d7ac716b281756405e53b
Emma <emmalivne.03@gmail.com>
2022-09-09 13:45:53 +0300

    Add PickCargo command

---
+++ src/main/java/frc/robot/subsystems/intake/commands/PickCargo.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems.intake.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.intake.Intake;
+
+public class PickCargo extends CommandBase {
+    private final Intake intake;
+    private final double power;
+
+    public PickCargo(double power) {
+        this.intake = Intake.getInstance();
+        this.power = power;
+        addRequirements(intake);
+    }
+
+    @Override
+    public void initialize() {
+        intake.openPiston();
+    }
+
+    @Override
+    public void execute() {
+        intake.setPower(power);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        intake.setPower(0);
+        intake.closePiston();
+    }
+}
