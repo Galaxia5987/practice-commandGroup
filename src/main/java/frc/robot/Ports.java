37cd03e7c31fb9e7450184dcfdabcb115f2a2954
Emma <emmalivne.03@gmail.com>
2022-09-09 13:45:12 +0300

    Add ports

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -1,6 +1,13 @@
 package frc.robot;
 
 public final class Ports {
+    public static class IntakePorts{
+        public static final int MOTOR = 0;
+        public static final int PISTON = 0;
+    }
 
+    public static class ConveyorPorts{
+        public static final int MOTOR = 0;
+    }
 
 }
