e70b3e8af4e17efa35c410e807e7c32465c042ac
Emma <emmalivne.03@gmail.com>
2022-09-09 13:46:10 +0300

    Add Conveyor class

---
+++ src/main/java/frc/robot/subsystems/conveyor/Conveyor.java
@@ -0,0 +1,33 @@
+package frc.robot.subsystems.conveyor;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Ports;
+
+public class Conveyor extends SubsystemBase {
+    private static Conveyor INSTANCE;
+    private final WPI_TalonFX motor = new WPI_TalonFX(Ports.ConveyorPorts.MOTOR);
+
+    private Conveyor() {
+        motor.enableVoltageCompensation(Constants.ENABLE_VOLT_COMP);
+        motor.configVoltageCompSaturation(Constants.CONFIG_VOLT_COMP);
+        motor.setInverted(Constants.CLOCKWISE);
+    }
+
+    public static Conveyor getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Conveyor();
+        }
+
+        return INSTANCE;
+    }
+
+    public double getPower() {
+        return motor.get();
+    }
+
+    public void setPower(double power) {
+        motor.set(power);
+    }
+}
