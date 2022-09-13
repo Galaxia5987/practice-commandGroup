66360bf42910ee3103e2c11394d12dc975b77b6b
Emma <emmalivne.03@gmail.com>
2022-09-13 16:24:51 +0300

    Add Feed command group

--- src/main/java/frc/robot/subsystems/commandgroups/Feed.java
+++ src/main/java/frc/robot/subsystems/commandgroups/Feed.java
@@ -1,6 +1,19 @@
 package frc.robot.subsystems.commandgroups;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.Constants;
+import frc.robot.subsystems.conveyor.Conveyor;
+import frc.robot.subsystems.conveyor.commands.Convey;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.intake.commands.PickCargo;
 
-public class Feed extends CommandBase {
+public class Feed extends ParallelCommandGroup {
+    public Feed(Intake PickCargo, Conveyor Convey){
+        addCommands(
+                new PickCargo(Constants.INTAKE_POWER),
+                new Convey(Constants.CONVEYOR_POWER)
+        );
+    }
+
 }
