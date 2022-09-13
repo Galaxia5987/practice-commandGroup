4cd1d2faed332d6b7d763ff6479e51d6be79a4b7
Emma <emmalivne.03@gmail.com>
2022-09-13 16:21:02 +0300

    Add constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,6 +3,7 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+import frc.robot.valuetuner.WebConstant;
 
 public final class Constants {
     public static final int CONFIG_VOLT_COMP = 10;
@@ -10,5 +11,7 @@ public final class Constants {
     public static final TalonFXInvertType CLOCKWISE = TalonFXInvertType.Clockwise;
     public static final boolean PISTON_CLOSE = false;
     public static final boolean PISTON_OPEN = true;
+    public static final double INTAKE_POWER = 0.5;
+    public static final double CONVEYOR_POWER = 0.5;
 
 }
