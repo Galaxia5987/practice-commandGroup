a0019e37657cd77c5c6679f558b3bccf5fff5410
Emma <emmalivne.03@gmail.com>
2022-09-13 16:57:05 +0300

    Add timer to PickCargo command

--- src/main/java/frc/robot/subsystems/intake/commands/PickCargo.java
+++ src/main/java/frc/robot/subsystems/intake/commands/PickCargo.java
@@ -1,11 +1,15 @@
 package frc.robot.subsystems.intake.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.intake.Intake;
+
+
 
 public class PickCargo extends CommandBase {
     private final Intake intake;
     private final double power;
+    private final Timer timer = new Timer();
 
     public PickCargo(double power) {
         this.intake = Intake.getInstance();
@@ -16,6 +20,8 @@ public class PickCargo extends CommandBase {
     @Override
     public void initialize() {
         intake.openPiston();
+        timer.reset();
+        timer.start();
     }
 
     @Override
@@ -27,5 +33,10 @@ public class PickCargo extends CommandBase {
     public void end(boolean interrupted) {
         intake.setPower(0);
         intake.closePiston();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return timer.get() > 5;
     }
 }
