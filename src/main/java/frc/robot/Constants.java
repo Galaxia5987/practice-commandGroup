e1334c4dfd8a7df18b41c10cec653e40e4381d29
Emma <emmalivne.03@gmail.com>
2022-09-13 18:38:56 +0300

    Add constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,9 +9,14 @@ public final class Constants {
     public static final int CONFIG_VOLT_COMP = 10;
     public static final boolean ENABLE_VOLT_COMP = true;
     public static final TalonFXInvertType CLOCKWISE = TalonFXInvertType.Clockwise;
+    public static final TalonFXInvertType COUNTER_CLOCKWISE = TalonFXInvertType.CounterClockwise;
     public static final boolean PISTON_CLOSE = false;
     public static final boolean PISTON_OPEN = true;
     public static final double INTAKE_POWER = 0.5;
     public static final double CONVEYOR_POWER = 0.5;
-
+    public static final int TALON_TIMEOUT = 10;//[ms]
+    public static final double kP = 0;
+    public static final double kI = 0;
+    public static final double kD = 0;
+    public static final double TICKS_PER_ROTATION = 2048;
 }
