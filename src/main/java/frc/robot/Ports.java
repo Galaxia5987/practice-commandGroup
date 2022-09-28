b8b02acf1ea5f1906c4ae16b7ca082d729a83d1b
Emma <emmalivne.03@gmail.com>
2022-09-28 19:04:02 +0300

    Add line

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -9,6 +9,7 @@ public final class Ports {
     public static class ConveyorPorts{
         public static final int MOTOR = 0;
     }
+
     public static class ShooterPorts{
         public static final int LEFT_MOTOR=0;
         public static final int RIGHT_MOTOR=0;
