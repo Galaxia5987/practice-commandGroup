696382b0a5631c5477e5434b1430e528cc149ff7
Emma <emmalivne.03@gmail.com>
2022-11-18 13:24:07 +0200

    Add conveyor and intake constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,21 +6,31 @@ import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
 import frc.robot.valuetuner.WebConstant;
 
 public final class Constants {
+    public static final int CONFIG_VOLT_COMP = 10;
+    public static final boolean ENABLE_VOLT_COMP = true;
+    public static final InvertType CLOCKWISE = InvertType.InvertMotorOutput;
+
     public static final class shooterConstants {
-        public static final int CONFIG_VOLT_COMP = 10;
-        public static final boolean ENABLE_VOLT_COMP = true;
         public static final TalonFXInvertType CLOCKWISE = TalonFXInvertType.Clockwise;
         public static final TalonFXInvertType COUNTER_CLOCKWISE = TalonFXInvertType.CounterClockwise;
-        public static final boolean PISTON_CLOSE = false;
-        public static final boolean PISTON_OPEN = true;
-        public static final double INTAKE_POWER = 0.5;
-        public static final double CONVEYOR_POWER = 0.5;
         public static final int TALON_TIMEOUT = 10;//[ms]
         public static final double kP = 0;
         public static final double kI = 0;
         public static final double kD = 0;
         public static final double TICKS_PER_ROTATION = 2048;
+        public static final double SHOOTER_VELOCITY = 0;
     }
+
+    public static final class intakeConstants {
+        public static final double INTAKE_POWER = 0.5;
+        public static final boolean PISTON_CLOSE = false;
+        public static final boolean PISTON_OPEN = true;
+    }
+
+    public static final class conveyorConstants {
+        public static final double CONVEYOR_POWER = 0.5;
+    }
+
     public static final class SwerveConstants{
         public static final int CONFIG_VOLT_COMP = 10;
         public static final boolean ENABLE_VOLT_COMP = true;
