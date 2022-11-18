b02230efa98f2e44d6384d78770f7a023e3e5d2a
Emma <emmalivne.03@gmail.com>
2022-11-18 13:25:47 +0200

    Fix

--- src/main/java/frc/robot/subsystems/commandgroups/Bit.java
+++ src/main/java/frc/robot/subsystems/commandgroups/Bit.java
@@ -3,17 +3,17 @@ package frc.robot.subsystems.commandgroups;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.subsystems.conveyor.Conveyor;
-import frc.robot.subsystems.conveyor.commands.Convey;
+import frc.robot.subsystems.conveyor.commands.ConveyUpper;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.intake.commands.PickCargo;
 
 public class Bit extends SequentialCommandGroup {
     public Bit(Intake PickCargo, Conveyor Convey){
         addCommands(
-                new PickCargo(Constants.INTAKE_POWER).withTimeout(5),
-                new PickCargo(-Constants.INTAKE_POWER).withTimeout(5),
-                new Convey(Constants.CONVEYOR_POWER).withTimeout(5),
-                new Convey(-Constants.CONVEYOR_POWER).withTimeout(5)
+                new PickCargo(Constants.intakeConstants.INTAKE_POWER).withTimeout(5),
+                new PickCargo(-Constants.intakeConstants.INTAKE_POWER).withTimeout(5),
+                new ConveyUpper(Constants.conveyorConstants.CONVEYOR_POWER).withTimeout(5),
+                new ConveyUpper(-Constants.conveyorConstants.CONVEYOR_POWER).withTimeout(5)
         );
     }
 }
