984b56ea6e2b8bb5c8d1d64e8b56dd2f656df5cb
Emma <emmalivne.03@gmail.com>
2022-11-18 13:24:49 +0200

    Add x button bind

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,9 +4,11 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.commandgroups.Feed;
+import frc.robot.subsystems.commandgroups.FeedShooter;
 import frc.robot.subsystems.commandgroups.Outtake;
 import frc.robot.subsystems.conveyor.Conveyor;
 import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.valuetuner.ValueTuner;
 import webapp.Webserver;
 
@@ -14,9 +16,11 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
     private final Conveyor conveyor = Conveyor.getInstance();
     private final Intake intake = Intake.getInstance();
+    private final Shooter shooter = Shooter.getInstance();
     private final XboxController xbox = new XboxController(0);
     private final JoystickButton a = new JoystickButton(xbox, XboxController.Button.kA.value);
     private final JoystickButton b = new JoystickButton(xbox, XboxController.Button.kB.value);
+    private final JoystickButton x = new JoystickButton(xbox, XboxController.Button.kX.value);
 
 
     /**
@@ -42,6 +46,7 @@ public class RobotContainer {
     private void configureButtonBindings() {
         a.whileHeld(new Feed(intake ,conveyor));
         b.whileHeld(new Outtake(intake, conveyor));
+        x.whileHeld(new FeedShooter(conveyor, shooter));
     }
 
 
