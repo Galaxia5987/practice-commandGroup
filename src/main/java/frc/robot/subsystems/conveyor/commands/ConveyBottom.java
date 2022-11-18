d55e2575543bb359859bedcd21c72ead1f8652bd
Emma <emmalivne.03@gmail.com>
2022-11-18 13:27:34 +0200

    Add convey bottom command

--- src/main/java/frc/robot/subsystems/conveyor/commands/ConveyBottom.java
+++ src/main/java/frc/robot/subsystems/conveyor/commands/ConveyBottom.java
@@ -1,2 +1,25 @@
-package frc.robot.subsystems.conveyor.commands;public class ConveyBottom {
+package frc.robot.subsystems.conveyor.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.conveyor.Conveyor;
+
+public class ConveyBottom extends CommandBase {
+    private final Conveyor conveyor;
+    private final double power;
+
+    public ConveyBottom(double power) {
+        this.conveyor = Conveyor.getInstance();
+        this.power = power;
+        addRequirements(conveyor);
+    }
+
+    @Override
+    public void execute() {
+        conveyor.setBottomPower(power);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        conveyor.setBottomPower(0);
+    }
 }
