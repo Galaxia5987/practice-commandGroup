0e8f8bcac9c604981a290e831e769728a0a24fc3
Emma <emmalivne.03@gmail.com>
2022-11-25 10:28:09 +0200

    Add SwerveDrive

--- src/main/java/frc/robot/subsystems/swerve/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveDrive.java
@@ -1,2 +1,16 @@
-package frc.robot.subsystems.swerve;public class SwerveDrive {
+package frc.robot.subsystems.swerve;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SwerveDrive extends SubsystemBase {
+    private static SwerveDrive ROBOT_ORIENTED_DRIVE = null;
+    private static SwerveDrive FIELD_ORIENTED_DRIVE = null;
+    private final SwerveModule[] modules = new SwerveModule[4];
+    private final SwerveDriveKinematics kinematics = new SwerveDriveKinematics();
+    private final SwerveDriveOdometry odometry = new SwerveDriveOdometry(kinematics, new Rotation2d());
+    private final
+
 }
