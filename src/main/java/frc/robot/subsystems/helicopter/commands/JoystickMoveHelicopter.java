11afc85c2f3375b889889bedfaacb913ff5516b4
Emma <emmalivne.03@gmail.com>
2022-11-30 19:50:34 +0200

    Add JoystickMoveHelicopter constructor

--- src/main/java/frc/robot/subsystems/helicopter/commands/JoystickMoveHelicopter.java
+++ src/main/java/frc/robot/subsystems/helicopter/commands/JoystickMoveHelicopter.java
@@ -1,2 +1,19 @@
-package frc.robot.subsystems.helicopter.commands;public class JoystickMoveHelicopter {
+package frc.robot.subsystems.helicopter.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.helicopter.Helicopter;
+
+public class JoystickMoveHelicopter extends CommandBase {
+    private final Helicopter helicopter;
+    private final double power;
+
+    public JoystickMoveHelicopter(Helicopter helicopter, double power) {
+        this.helicopter = helicopter;
+        this.power = power;
+        addRequirements(helicopter);
+    }
+
+    @Override
+    public void execute() {
+    }
 }
