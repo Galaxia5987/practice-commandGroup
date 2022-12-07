39900c242e9a49c54a24887aa5ded56b382caf3d
Emma <emmalivne.03@gmail.com>
2022-12-07 08:59:05 +0200

    Add Helicopter bit to Bit

--- src/main/java/frc/robot/subsystems/commandgroups/Bit.java
+++ src/main/java/frc/robot/subsystems/commandgroups/Bit.java
@@ -5,6 +5,8 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.subsystems.conveyor.Conveyor;
 import frc.robot.subsystems.conveyor.commands.ConveyUpper;
+import frc.robot.subsystems.helicopter.Helicopter;
+import frc.robot.subsystems.helicopter.commands.SetPosition;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.intake.commands.PickCargo;
 import frc.robot.subsystems.shooter.Shooter;
@@ -14,7 +16,7 @@ import frc.robot.subsystems.swerve.commands.SetAnglePower;
 import frc.robot.subsystems.swerve.commands.SetDrivePower;
 
 public class Bit extends SequentialCommandGroup {
-    public Bit(Intake PickCargo, Conveyor Convey, Shooter Shoot, SwerveDrive SetDrivePower, SwerveDrive SetAnglePower){
+    public Bit(Intake PickCargo, Conveyor Convey, Shooter Shoot, SwerveDrive SetDrivePower, SwerveDrive SetAnglePower, Helicopter SetPosition){
         addCommands(
                 new PickCargo(Constants.intakeConstants.INTAKE_POWER).withTimeout(5),
                 new PickCargo(-Constants.intakeConstants.INTAKE_POWER).withTimeout(5),
@@ -22,7 +24,8 @@ public class Bit extends SequentialCommandGroup {
                 new ConveyUpper(-Constants.conveyorConstants.CONVEYOR_POWER).withTimeout(5),
                 new Shoot(2500, new Rotation2d(0)).withTimeout(5),
                 new SetDrivePower(50).withTimeout(5),
-                new SetAnglePower(50).withTimeout(5)
+                new SetAnglePower(50).withTimeout(5),
+                new SetPosition(new Rotation2d(0)).withTimeout(5)
         );
     }
 }
